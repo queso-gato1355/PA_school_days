e1e4b3cf839255143069f36d8b0f1a13002f022e
queso-gato1355 <solar767@unist.ac.kr>
2024-06-20 22:39:34 +0900

    Edit metadata

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,26 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
     title: "PA School Days",
     description: "PA characters are going to school!",
+    openGraph: {
+        title: "PA School Days",
+        description: "PA characters are going to school!",
+        type: "website",
+        url: "https://pa-school-days.vercel.app", // 실제 웹사이트 URL로 변경하세요
+        images: [
+          {
+            url: "https://picsum.photos/630/1200", // Open Graph 이미지 URL
+            width: 1200,
+            height: 630,
+            alt: "PA School Days",
+          },
+        ],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: "PA School Days",
+        description: "PA characters are going to school!",
+        images: ["https://picsum.photos/630/1200"], // Twitter 카드 이미지 URL
+      },
 };
 
 export default function RootLayout({ children }) {
